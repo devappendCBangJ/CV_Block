fd2e36cdb8a9474c284042f10d85387a06850ae1
dev.appendCBangJ <dev.appendCBangJ@gmail.com>
2023-09-01 16:34:49 +0900

    Add Simultaneously Change bbox and seg Code

--- Data/Label Prerprocessing/6_2_YOLO_BboxSeg_Text_Multiple_Label_Change_For_Train.py
+++ Data/Label Prerprocessing/6_2_YOLO_BboxSeg_Text_Multiple_Label_Change_For_Train.py
@@ -11,62 +11,69 @@ import argparse
 # ==============================================================
 # 0. 변수 정의
 # ==============================================================
-parser = argparse.ArgumentParser(description='6_YOLOv5_Text_Multiple_Label_Change_For_Train')
+parser = argparse.ArgumentParser(description='6_2_YOLO_BboxSeg_Text_Multiple_Label_Change_For_Train')
 
-parser.add_argument('--base-path', default='/media/hi/SK Gold P31/Capstone/GolfBall/Golfball_Near/labels', type=str, help='변경할 라벨들이 모여있는 폴더 지정')
+parser.add_argument('--base-path', default='/media/hi/SK Gold P31/Capstone/GolfBall/Golfball_Near/labels', type=str, help='변경할 다양한 종류의 라벨 폴더가 모여있는 부모 폴더 지정')
+parser.add_argument('--bbox-seg-path', default=['Bbox', 'Seg'], type=str, help='각각의 종류의 라벨 폴더 내에 있는 Bbox, Seg 폴더명 지정')
 parser.add_argument('--before-label', default="all", type=str, help='변경 이전 라벨 지정')
 parser.add_argument('--after-label', default="0", type=str, help='변경 이후 라벨 지정')
 
 args = parser.parse_args()
 
-unique_label = []
+unique_cls = []
 
 # ==============================================================
 # 1. Label 파일명 추출 + Label 수정 (base_path -> train_path -> 각 label 변경)
 # ==============================================================
-def revise_label(labels_path, before_label, after_label):
+def get_filenames(folder_path):
+    filenames = os.listdir(folder_path)
+    return filenames
+
+def revise_label(labels_path, before_cls, after_cls):
     # 1) Label 파일명 추출
     for label_path in glob.glob(os.path.join(labels_path, '*.txt')):
         with open(label_path, 'r') as f:
             # 2) label 한줄씩 불러오기
             lines = f.readlines()
 
-            # 3) label, bbox 값 확인
+            # 3) class, label 값 확인
             for line in lines:
-                label, bbox = line.split(' ', maxsplit=1)
+                cls, label = line.split(' ', maxsplit=1)
                 # (1) Unique label 저장
-                if label not in unique_label:
-                    unique_label.append(label)
+                if cls not in unique_cls:
+                    unique_cls.append(cls)
                 # (2) before_label인 경우 확인
-                if before_label != "all" and label == before_label:
-                    print(f'labels_path : {label_path} | label : {label}')
+                if before_cls != "all" and cls == before_cls:
+                    print(f'labels_path : {label_path} | class : {cls}')
                 """
                 # (3) 모든 경우 확인
-                print(f'labels_path : {label_path} | label : {label}')
+                print(f'labels_path : {label_path} | class : {cls}')
                 """
 
         with open(label_path, 'w') as f:
             # 4) label 변환
             for line in lines:
                 # (1) label Split
-                label, bbox = line.split(' ', maxsplit=1)
+                cls, label = line.split(' ', maxsplit=1)
                 # print(f'labels_path : {label_path} | label : {label}')
                 # (2) label 변환
                 # 1] 전부 변환하는 경우
-                if before_label == "all":
-                    f.write(f'{after_label} {bbox}')
+                if before_cls == "all":
+                    f.write(f'{after_cls} {label}')
                 # 2] 일부만 변환하는 경우
                 else:
-                    if label == before_label:
-                        f.write(f'{after_label} {bbox}')
+                    if cls == before_cls:
+                        f.write(f'{after_cls} {label}')
                     else:
-                        f.write(f'{label} {bbox}')
+                        f.write(f'{cls} {label}')
 
-    print(f'before unique_label : {unique_label}')
+    print(f'before unique_label : {unique_cls}')
 
 # ==============================================================
 # 2. Main문
 # ==============================================================
-for idx, f_path in enumerate(['train/', 'val/', 'test/']):
-    print(f'f_path : {f_path}')
-    revise_label(f'{args.base_path}/{f_path}', before_label = args.before_label, after_label = args.after_label)
+base_folder_list = get_filenames(args.base_path)
+for bf_path in base_folder_list:
+    for bs_path in args.bbox_seg_path:
+        print(f'f_path : {bf_path}/{bs_path}')
+        revise_label(f'{args.base_path}/{bf_path}/{bs_path}', before_label = args.before_label, after_label = args.after_label)
